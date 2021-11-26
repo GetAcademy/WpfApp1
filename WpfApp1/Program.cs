6cd7233f9ded3b8f1982d2dd989a2bc296b122e6
GET Academy <terje@getacademy.no>
2021-11-26 10:35:46 +0100

    Initial commit

---
+++ WpfApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var panel = new StackPanel();
+            var label = new Label() { Content = "0" };
+            var button = new Button() { Content = "Trykk her!" };
+            panel.Children.Add(label);
+            panel.Children.Add(button);
+            window.Content = panel;
+            app.Run(window);
+        }
+    }
+}
