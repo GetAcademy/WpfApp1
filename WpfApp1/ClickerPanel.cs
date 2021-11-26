6bf296171a58e4abd8a49ffd25f697befe5d9d2e
GET Academy <terje@getacademy.no>
2021-11-26 11:06:24 +0100

    x

---
+++ WpfApp1/ClickerPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class ClickerPanel
+    {
+        static Label label;
+        int count = 0;
+        public StackPanel Panel { get; }
+
+        public ClickerPanel()
+        {
+            Panel = new StackPanel();
+            label = new Label() { Content = "0" };
+            var button = new Button() { Content = "Trykk her!" };
+            button.Click += ButtonClick;
+            Panel.Children.Add(label);
+            Panel.Children.Add(button);
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            count++;
+            label.Content = count;
+        }
+    }
+}
--- WpfApp1/Program.cs
+++ WpfApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -6,18 +7,62 @@ namespace WpfApp1
 {
     class Program
     {
+    
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+
+            var clickerPanel1 = new ClickerPanel();
+            var clickerPanel2 = new ClickerPanel();
+            var clickerPanel3 = new ClickerPanel();
+            var clickerPanel4 = new ClickerPanel();
+            var clickerPanel5 = new ClickerPanel();
+
+            var stackPanel = new StackPanel();
+            stackPanel.Children.Add(clickerPanel1.Panel);
+            stackPanel.Children.Add(clickerPanel2.Panel);
+            stackPanel.Children.Add(clickerPanel3.Panel);
+            stackPanel.Children.Add(clickerPanel4.Panel);
+            stackPanel.Children.Add(clickerPanel5.Panel);
+
+            window.Content = stackPanel;
+
+
+            app.Run(window);
+        }
+
+
+        /*
+class Program
+    {
+        static Label label;
+        static int count = 0;
+
+
         [STAThread]
         public static void Main(string[] args)
         {
             var app = new Application();
             var window = new Window();
             var panel = new StackPanel();
-            var label = new Label() { Content = "0" };
+            label = new Label() { Content = "0" };
             var button = new Button() { Content = "Trykk her!" };
+            button.Click += ButtonClick;
             panel.Children.Add(label);
             panel.Children.Add(button);
             window.Content = panel;
             app.Run(window);
         }
+
+        private static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            count++;
+            label.Content = count;
+        }
+    }         
+         */
     }
 }
